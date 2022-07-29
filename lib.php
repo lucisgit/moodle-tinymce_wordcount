2817deebd3e1f5f98dfbbbd26a715c031e6142fe
Tony Butler <tonyjbutler@gmail.com>
2022-07-29 19:02:23 +0100

    ITS-8834 tinymce_wordcount: Address issues identified by codechecker

--- lib.php
+++ lib.php
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Plugin for counting words in TinyMCE (Moodle-specific modified version).
  *
